7d7b80fab4bc3e871d01f93a63f054ce0096a0a4
abhinendra pratap <123157777+Mohit1302a@users.noreply.github.com>
2026-06-21 22:25:45 +0530

    Add files via upload

---
+++ basic1.py
@@ -0,0 +1,35 @@
+## use double hash for single line comment 
+## use triple double quotes for multi line comment
+## this is a single line comment
+"""this is a multi line comment
+this is a multi line comment
+this is a multi line comment
+this is a multi line comment
+"""
+print("Hello World")
+indentation is important in python
+if 5 > 2:
+    print("Five is greater than two!")
+
+##line continuation
+a = 1 + 2 + 3 + \
+    4 + 5 + 6
+print(a)
+
+#variables assignments
+x = 5
+y = "Hello, World!"
+print(x)
+print(y)
+
+#type of variables
+print(type(x))
+print(type(y))  
+
+#type inference
+a = 10
+b = 3.14
+c = "Python"
+print(type(a))
+print(type(b))
+print(type(c))
